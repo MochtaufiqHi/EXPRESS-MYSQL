c57e2d9add7537b8c2852798a4322b1b22de957b
mochtaufiqidayat <mochammadtaufiq779@gmail.com>
2023-06-27 00:35:38 +0700

    postman

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const express = require("express")
 
 const app = express()
 
+// app.method(path, handler)
+// app.use("/", (req, res, next) => {
+//   res.send('Hello World!');
+// })
+
+app.get("/", (req, res) => {
+  res.send('Hello get metod');
+})
+
+app.post("/", (req, res) => {
+  res.send('Hello post method')
+})
+
 app.listen(8000, () => {
   console.log('Server berhasil berjalan pada port 4000')
 })
