38325452d705a4ed9cd1d71244d7003e029d6758
mochtaufiqidayat <mochammadtaufiq779@gmail.com>
2023-06-27 10:12:33 +0700

    add-middleware

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ const usersRoutes = require('./routes/users')
 
 const app = express()
 
+const middlewareLogRequest = require('./middleware/logs')
+
+// middleware
+app.use((req, res, next) => {
+  console.log('log terjadi pada request ke API')
+  next();
+})
+
+app.use(middlewareLogRequest)
+// router users
 app.use('/users', usersRoutes)
 
-// app.method(path, handler)
-// app.use("/", (req, res, next) => {
-//   res.send('Hello World!');
-// })
+
 
 app.get("/", (req, res) => {
   res.json({
