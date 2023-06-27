ae2b108e854ce8e4998767763f6f1d0a92d3923d
mochtaufiqidayat <mochammadtaufiq779@gmail.com>
2023-06-27 23:05:14 +0700

    add:CRUD

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,55 +1,82 @@
-const UsersModel = require('../models/users')
+const UsersModel = require("../models/users");
 
 // controller for users
 const getAllUsers = async (req, res) => {
-    try {
-      
-      const [data] = await UsersModel.getAllUsers();
-  
-      res.json({
-        message: 'Get all users success',
-        data: data
-      })
-    } catch (error) {
-      res.status(500).json({
-        message: 'Internal Server Error',
-        serverMessage: error,
-      })
-    }
-}
+  try {
+    const [data] = await UsersModel.getAllUsers();
 
-const createNewUsers = (req, res) => {
-  console.log(req.body)
-  res.json({
-    message: 'Create all users success',
-    data: req.body
-  })
-}
+    res.json({
+      message: "Get all users success",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      serverMessage: error,
+    });
+  }
+};
 
-const updateUser = (req, res) => {
-  const { idUser } = req.params
-  console.log('idUser :', idUser);
-  res.json({
-    message: 'Update user success',
-    data: req.body
-  })
-}
+const createNewUsers = async (req, res) => {
+  const { body } = req;
 
-const deleteUser = (req, res) => {
-  const { idUser } = req.params
-  console.log('idUser :', idUser)
-  res.json({
-    message: 'Delete user success',
-    data: {
-      id: idUser,
-      name: "wisnu hamka"
-    }
-  })
-}
+  try {
+    await UsersModel.createNewUser(body);
+    res.json({
+      message: "Create all users success",
+      data: body,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      serverMessage: error,
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { idUser } = req.params;
+  const { body } = req;
+
+  try {
+    await UsersModel.updateUser(body, idUser)  
+    res.json({
+      message: "Update user success",
+      data: {
+        id: idUser,
+        ...body
+      }
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      serverMessage: error,
+    });
+  }
+
+};
+
+const deleteUser = async (req, res) => {
+  const { idUser } = req.params;
+
+  try {
+    await UsersModel.deleteUser(idUser)
+    res.json({
+      message: "Delete user success",
+      data: null
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      serverMessage: error,
+    });
+  }
+   
+};
 
 module.exports = {
   getAllUsers,
   createNewUsers,
   updateUser,
-  deleteUser
-}
+  deleteUser,
+};
